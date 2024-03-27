928908c7e95c3b10b5d024cf8bd2edaa00ac9abe
Cooper Urich <curich@expediagroup.com>
2024-03-26 19:25:12 -0500

    page fault update

--- aos_pintos/src/userprog/exception.c
+++ aos_pintos/src/userprog/exception.c
@@ -141,13 +141,11 @@ static void page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  if (!user)
-    {
-      f->eip = f->eax;
-      f->eax = -1;
-      return;
-    }
-
+if (user && not_present) {
+  if (!page_in (fault_addr))
+    thread_exit ();
+  return;
+}
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
